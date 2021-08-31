0f21f1a7d2475ea348963396a8e938ee54731eac
Herman Benao <hermben@gmail.com>
2021-08-31 14:27:47 -0700

    adds logic to essentialTrainingTests.cs

--- EssentialTraining/Class1.cs
+++ EssentialTraining/Class1.cs
@@ -4,5 +4,9 @@ namespace EssentialTraining
 {
     public class Class1
     {
+        public object AddTwo(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
